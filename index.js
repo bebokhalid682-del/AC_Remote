efdd53d812c0cd9038b6d166212ef23db939a578
Khaled_EL_Gohary <bebokhalid682@gmail.com>
2026-02-07 00:59:16 +0200

    AC_Remote

--- index.js
+++ index.js
@@ -28,3 +28,25 @@ function decreseTemp(){
     tempreture--;
     temp.textContent = tempreture;
 }
+let click = 1 ;
+function modeSwitch(){
+    if(stuts == false){
+        return;
+    }
+    let modeOne = document.getElementById("mode_one");
+    let modeTwo = document.getElementById("mode_two");
+    let modeThree = document.getElementById("mode_three");
+    if(click == 1){
+        modeOne.classList.add("invisible");
+        modeTwo.classList.remove("invisible");
+        click++;
+    }else if(click == 2){
+        modeTwo.classList.add("invisible");
+        modeThree.classList.remove("invisible");
+        click++;
+    }else if(click == 3){
+        modeOne.classList.remove("invisible");
+        modeThree.classList.add("invisible");
+        click = 1;
+    }
+}
